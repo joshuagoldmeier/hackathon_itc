7d3441dbd85a6d49c008ab71056dc9154d2dc872
Yonah Taurog <xtreemtg888@Yonahs-MacBook-Pro2017.local>
2018-06-20 19:55:28 +0300

    final

--- Sports_Equipment.py
+++ Sports_Equipment.py
@@ -1,4 +1,29 @@
-ski = {
-    'Beginner': ["Gloves", "Skis", "Boots", "Coat", "Goggles"],
-    'Expert': ["Helmet", "Poles"]
+sports = {
+    'ski' : {
+        'Beginner': ["Ski_Gloves", "Skis", "Ski_Boots", "Ski_Coat", "Ski_Goggles"],
+    },
+
+    'baseball' : {
+        'Beginner': ["Baseball_Bat", "Baseballs", "Baseball_Helmet", "Baseball_Glove"],
+    },
+
+    'hockey' : {
+        'Beginner': ["Hockey_stick", "Hockey_puck", "Hockey_Ice_Skates", "Hockey_Helmet"]
+    },
+
+    'basketball' : {
+        "Beginner": ["Basketball", 'Basketball_hoop', "Basketball_net"]
+    },
+
+    'football' : {
+        "Beginner": ["Football", "Football_helmet", "Football_shoulder_pads",  ]
+    },
+
+    'soccer' : {
+        "Beginner": ["Soccer_ball", "Soccer_cleats", "Adult_shin_guards"]
+    },
+
+    'fencing' : {
+    "Beginner": ["Fencing_sword", "Fencing_jacket", "Fencing_pants", "Fencing_chest_guard", "Fencing_mask", "Fencing_Lame"]
+    }
 }
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-from Sports_Equipment import ski
 from bottle import route, run, template, get
 import bottle as b
+from pymongo import MongoClient
+import json
+
+
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
 
 
 @get("/")
 def index():
     return template("index.html")
 
-
-keyWord = ""
-walmart_api = 'http://api.walmartlabs.com/v1/search?apiKey=utuc2y44uxauxzqk985vft4z&query="' + keyWord + '"'
-
-
-# @route('/hello/<name>')
-# def index(name):
-#     return template('<b>Hello {{name}}</b>!', name=name)
-#
-# run(host='localhost', port=8080)
-
-for item in ski["Beginner"]:
-    print("ski ", item)
-
+@get("/sport/<sport_name>")
+def index(sport_name):
+    client = MongoClient('localhost', 27017)
+    db = client.hackathon
+    items = db.items.find()
+    print(items)
+    for item in items:
+        if item["_id"] == sport_name:
+            return item
+    return "can't find sport!"
 
 @get('/js/<filename:re:.*\.js>')
 def javascripts(filename):
