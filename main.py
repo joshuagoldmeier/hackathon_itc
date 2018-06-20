3004724a9c733e52d326dd0ef27bbeb71802b6a7
Yonah Taurog <xtreemtg888@Yonahs-MacBook-Pro2017.local>
2018-06-20 12:31:26 +0300

    my changed

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 run(host='localhost', port=8080)
+
+
+keyWord = ""
+walmart_api = 'http://api.walmartlabs.com/v1/search?apiKey=utuc2y44uxauxzqk985vft4z&query="' + keyWord + '"'
